298a14313d485fc022ace686193ab4ea67e69678
Leonardo <leo-nino70@outlook.com>
2026-09-23 00:50:34 -0300

    first commit

---
+++ src/test/java/dev/java10x/CadatroDeNinjas/CadatroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadatroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadatroDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
